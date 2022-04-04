8b175ffe4bb6f098c5dd9f8eb58563182f06084c
antfor <38752136+antfor@users.noreply.github.com>
2022-04-04 15:46:49 +0200

    städa

--- app/given.hs
+++ app/given.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import Data.List
-import System.Random
-import Criterion.Main
-import Control.Parallel
-import Control.Parallel.Strategies
-import Control.DeepSeq
-import System.Environment
-import qualified Control.Monad.Par as MP
+import           Data.List
+import           System.Random
+import           Criterion.Main
+import           Control.Parallel
+import           Control.Parallel.Strategies
+import           Control.DeepSeq
+import           System.Environment
+import qualified Control.Monad.Par             as MP
 -- code borrowed from the Stanford Course 240h (Functional Systems in Haskell)
 -- I suspect it comes from Bryan O'Sullivan, author of Criterion
 
@@ -16,31 +16,50 @@ data T a = T !a !Int
 
 mean :: (RealFrac a) => [a] -> a
 mean = fini . foldl' go (T 0 0)
-  where
-    fini (T a _) = a
-    go (T m n) x = T m' n'
-      where m' = m + (x - m) / fromIntegral n'
-            n' = n + 1
+ where
+  fini (T a _) = a
+  go (T m n) x = T m' n'
+   where
+    m' = m + (x - m) / fromIntegral n'
+    n' = n + 1
 
 
 resamples :: Int -> [a] -> [[a]]
 resamples k xs =
-    take (length xs - k) $
-    zipWith (++) (inits xs) (map (drop k) (tails xs))
+  take (length xs - k) $ zipWith (++) (inits xs) (map (drop k) (tails xs))
 
 
 jackknife :: ([a] -> b) -> [a] -> [b]
 jackknife f = map f . resamples 500
 
-crud = zipWith (\x a -> sin (x / 300)**2 + a) [0..]
+crud = zipWith (\x a -> sin (x / 300) ** 2 + a) [0 ..]
+
 
 main = do
   args <- getArgs
   mapM_ putStrLn args
+  let fun = head args
+  case fun of
+    "b" -> benchmain (tail args)
+    "s" -> singelmain (tail args)
+    _   -> benchmain args
+
+singelmain args = do
+  let n = read (head args) :: Int
+  let (xs, ys) =
+        splitAt 1500 (take 6000 (randoms (mkStdGen 211570155)) :: [Float])
+  -- handy (later) to give same input different parallel functions
+  let rs = crud xs ++ ys
+  putStrLn "B2"
+  print $ sum (jackA mean rs)
+  putStrLn "Done"
+
+benchmain args = do
+
   let n = read (head args) :: Int
 
-  let (xs,ys) = splitAt 1500  (take 6000
-                               (randoms (mkStdGen 211570155)) :: [Float] )
+  let (xs, ys) =
+        splitAt 1500 (take 6000 (randoms (mkStdGen 211570155)) :: [Float])
   -- handy (later) to give same input different parallel functions
 
   let rs = crud xs ++ ys
@@ -52,118 +71,112 @@ main = do
   print $ sum rs
   print n
  -- print $ jackknife mean rs == jackB mean rs
-  withArgs (drop 1 args) $ defaultMain
-        [
+  withArgs (drop 1 args) $ defaultMain [
 -- {-
-    --      bench "jackknife" (nf (jackknife mean) rs),
-        --  bench "A" (nf (jackA mean) rs)
-    --      bench "B" (nf (jackB n mean) rs),
-          -- bench "B" (nf (jackB n mean) rs)
-          --bench "A'" (nf (jackA2 n mean) rs)
-           --bench "A'" (nf (jackA2 n mean) rs),
-    --      bench "A" (nf (jackA mean) rs)
-    --       bench "B" (nf (jackB n mean) rs)
-    --     bench "Be2" (nf (jackBe2 mean) rs),
-    --     bench "Be" (nf (jackBe n mean) rs)
- -- -}     
+          bench "jackknife" (nf (jackknife mean) rs),
+          
+          bench "A" (nf (jackA mean) rs),
+          bench "ADiv" (nf (jackADiv 4 mean) rs),
+          bench "AChunk" (nf (jackAChunk n mean) rs)
+
+ -- -}
             --bench "C'" (nf (jackCparmap' mean) rs)
             --bench "C" (nf (jackCparmap mean) rs),
             --bench "jackC" (nf (jackC mean) rs)
             --bench "D" (nf (jackD mean) rs)
-            bench "DPar" (nf (jackDpar mean) rs)
-         ]
+            --bench "DPar" (nf (jackDpar mean) rs)
+                                                                          ]
 
 
 
-
+--A
 jackA :: (NFData b) => ([a] -> b) -> [a] -> [b]
 jackA f = amap f . resamples 500
 
-jackA' ::(NFData b) => Int -> ([a] -> b) -> [a] -> [b]
-jackA' n f =  divideMap n (amap (map f)) . resamples 500
+jackADiv :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
+jackADiv n f = divideMap n (amap (map f)) . resamples 500
 
-jackA2 ::(NFData b) => Int -> ([a] -> b) -> [a] -> [b]
-jackA2 n f =  chunkMap n (amap (map f)) . resamples 500
+jackAChunk :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
+jackAChunk n f = chunkMap n (amap (map f)) . resamples 500
 
-jackB :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
-jackB n f = divideMap n (runEval . bmap (map f)) . resamples 500
+-- B
 
-jackBe :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
-jackBe n f = divideMap n (MP.runPar . MP.parMap (map f)) . resamples 500
+jackB :: (NFData b) => ([a] -> b) -> [a] -> [b]
+jackB f = bmap f . resamples 500
 
-jackBe2 :: (NFData b) =>  ([a] -> b) -> [a] -> [b]
-jackBe2 f = MP.runPar . MP.parMap f . resamples 500
+jackBDiv :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
+jackBDiv n f = divideMap n (bmap (map f)) . resamples 500
 
+jackBChunk :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
+jackBChunk n f = chunkMap n (bmap (map f)) . resamples 500
+
+--Be
+
+jackBeDiv :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
+jackBeDiv n f = divideMap n (MP.runPar . MP.parMap (map f)) . resamples 500
+
+jackBeChunk :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
+jackBeChunk n f = chunkMap n (MP.runPar . MP.parMap (map f)) . resamples 500
+
+jackBe :: (NFData b) => ([a] -> b) -> [a] -> [b]
+jackBe f = MP.runPar . MP.parMap f . resamples 500
+
+-- C
 jackC :: (NFData b) => ([a] -> b) -> [a] -> [b]
 jackC f = cmap f . resamples 500
 
-jackCparmap :: (NFData b) => ([a] -> b) -> [a] -> [b]
-jackCparmap f = cparmap f . resamples 500
+jackCDiv :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
+jackCDiv n f = divideMap n (cmap (map f)) . resamples 500
 
-jackCparmap' :: (NFData b) => ([a] -> b) -> [a] -> [b]
-jackCparmap' f = cparmap' f . resamples 500
+jackCChunk :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
+jackCChunk n f = chunkMap n (cmap (map f)) . resamples 500
 
+-- D
 jackD :: (NFData b) => ([a] -> b) -> [a] -> [b]
 jackD f = dmap f . resamples 500
 
-jackDpar :: (NFData b) => ([a] -> b) -> [a] -> [b]
-jackDpar f rs = MP.runPar $ do
-                    parMapM (return . f) (resamples 500 rs)
+jackDDiv :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
+jackDDiv n f = divideMap n (dmap (map f)) . resamples 500
+
+jackDChunk :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
+jackDChunk n f = chunkMap n (dmap (map f)) . resamples 500
 
 
-
-amap :: (NFData b) => (a -> b) -> [a] -> [b] 
-amap f []     = []
-amap f (a:as) = par b $ pseq bs (b:bs)
+amap :: (NFData b) => (a -> b) -> [a] -> [b]
+amap f []       = []
+amap f (a : as) = par b $ pseq bs (b : bs)
  where
-     b  = force $ f a
-     bs = amap f as
+  b  = force $ f a
+  bs = amap f as
 
 
-bmap :: (NFData b) => (a -> b) -> [a] -> Eval [b]
-bmap f []     = return []
-bmap f (a:as) = do
-              b   <- rpar $ force (f a)
-              bs  <- bmap f as
-              rseq bs
-              rseq b
-              return (b:bs)
+bmap :: (NFData b) => (a -> b) -> [a] -> [b]
+bmap f []       =  []
+bmap f (a : as) = runEval $ do
+  b  <- rpar $ force (f a)
+  let bs = bmap f as
+  rseq bs
+  rseq b
+  return (b : bs)
+
 
 cmap :: (NFData b) => (a -> b) -> [a] -> [b]
-cmap f []     = []
-cmap f (a:as) = (b:bs) `using` strat
-    where
-        b = force (f a)
-        bs = cmap f as
-        strat v = do rpar b; rseq bs; return v
+cmap f xs = map f xs `using` parList rdeepseq
 
-cparmap ::  (a -> b) -> [a] -> [b]
-cparmap f xs = map f xs `using` parList rseq
 
-cparmap' :: (NFData b) => (a -> b) -> [a] -> [b]
-cparmap' f xs = map f xs `using` parList rdeepseq
-
-dmap :: (NFData b) => (a -> b) -> [a] -> [b]
-dmap f []     = []
-dmap f (a:as) = MP.runPar $ do
-    i <- MP.new
-    j <- MP.new
-    MP.fork $ MP.put i (f a)
-    MP.fork $ MP.put j (dmap f as)
-    b <- MP.get i
-    bs <- MP.get j
-    return $ b:bs
-
-parMapM :: NFData b => (a -> MP.Par b) -> [a] -> MP.Par [b]
-parMapM f as = do
-    ibs <- mapM (MP.spawn . f) as
-    mapM MP.get ibs
+dmap :: NFData b => (a -> b) -> [a] -> [b]
+dmap f as = MP.runPar $ do
+      ibs <- mapM (MP.spawn . return . f) as
+      mapM MP.get ibs
 
 chunk :: Int -> [a] -> [[a]]
 chunk n [] = []
-chunk n xs = as : chunk n bs
-    where
-        (as,bs) = splitAt n xs
+chunk n xs = as : chunk n bs where (as, bs) = splitAt n xs
+
+
+chunkMap :: (NFData b) => Int -> ([[a]] -> [[b]]) -> [a] -> [b]
+chunkMap n mapF = concat . mapF . chunk n
+
 
 divide :: Int -> [a] -> [[a]]
 divide n xs = chunk d xs
@@ -173,5 +186,13 @@ divide n xs = chunk d xs
 divideMap :: (NFData b) => Int -> ([[a]] -> [[b]]) -> [a] -> [b]
 divideMap n mapF = concat . mapF . divide n
 
-chunkMap :: (NFData b) => Int -> ([[a]] -> [[b]]) -> [a] -> [b]
-chunkMap n mapF = concat . mapF . chunk n
+-- 2 ------------------------------------------------------------
+{-
+divConq :: (a -> b) -> a -> (a -> Maybe [a]) -> ([b] -> b) -> b
+divConq f arg divide combine | isJust newArg = combine $ map f (devide arg)
+                             | otherwise     = undefined
+    where
+        newArg = devide arg
+
+
+-}
