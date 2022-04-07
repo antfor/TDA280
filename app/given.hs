be69a29995bac2e948f3cc5d2edd555b8a4a2e16
tehAndrew <andpa_kontakt@live.se>
2022-04-07 13:57:01 +0200

    First iteration of code cleaning

--- app/given.hs
+++ app/given.hs
@@ -13,7 +13,6 @@ import qualified Control.Monad.Par             as MP
 
 data T a = T !a !Int
 
-
 mean :: (RealFrac a) => [a] -> a
 mean = fini . foldl' go (T 0 0)
  where
@@ -23,26 +22,23 @@ mean = fini . foldl' go (T 0 0)
     m' = m + (x - m) / fromIntegral n'
     n' = n + 1
 
-
 resamples :: Int -> [a] -> [[a]]
 resamples k xs =
   take (length xs - k) $ zipWith (++) (inits xs) (map (drop k) (tails xs))
-
 
 jackknife :: ([a] -> b) -> [a] -> [b]
 jackknife f = map f . resamples 500
 
 crud = zipWith (\x a -> sin (x / 300) ** 2 + a) [0 ..]
 
-
 main = do
   args <- getArgs
   mapM_ putStrLn args
   let fun = head args
   case fun of
-    "b" -> benchmain (tail args)
-    "s" -> singelmain (tail args)
-    _   -> benchmain args
+    "b1" -> benchmain1 (tail args)
+    "b2" -> benchmain2 (tail args)
+    _   -> putStrLn "wdwd"
 
 singelmain args = do
   let n = read (head args) :: Int
@@ -52,86 +48,55 @@ singelmain args = do
   let rs = crud xs ++ ys
   putStrLn "B2"
   print $ length rs
-  print $ sum (mergeSort n   rs)
+  print $ sum (mergeSort True n rs)
   putStrLn "Done"
 
-benchmain args = do
-
+benchmain1 args = do
   let n = read (head args) :: Int
 
   let (xs, ys) =
         splitAt 1500 (take 6000 (randoms (mkStdGen 211570155)) :: [Float])
-  -- handy (later) to give same input different parallel functions
 
   let rs = crud xs ++ ys
---  putStrLn $ "sample mean:    " ++ show (mean rs)
+  putStrLn $ "sample mean:    " ++ show (mean rs)
 
-  --let j = jackknife mean rs :: [Float]
-  --putStrLn $ "jack mean min:  " ++ show (minimum j)
-  --putStrLn $ "jack mean max:  " ++ show (maximum j)
+  let j = jackknife mean rs :: [Float]
+  putStrLn $ "jack mean min:  " ++ show (minimum j)
+  putStrLn $ "jack mean max:  " ++ show (maximum j)
   print $ sum rs
-  print n
-  let ls = replicate 100000 1 ++ [0] :: [Int]
-  print $ sum ls
- -- print $ jackknife mean rs == jackB mean rs
+
   withArgs (drop 1 args) $ defaultMain [
+    bench "jackknife" (nf (jackknife mean) rs),
+    bench "jackknifeA" (nf (jackA mean) rs),
+    bench "jackknifeAChunk" (nf (jackAChunk n mean) rs),
+    bench "jackknifeB" (nf (jackB mean) rs),
+    bench "jackknifeBChunk" (nf (jackBChunk n mean) rs),
+    bench "jackknifeBe" (nf (jackBe mean) rs),
+    bench "jackknifeBeChunk" (nf (jackBeChunk n mean) rs),
+    bench "jackknifeC" (nf (jackC mean) rs),
+    bench "jackknifeCChunk" (nf (jackCChunk n mean) rs),
+    bench "jackknifeD" (nf (jackD mean) rs),
+    bench "jackknifeDChunk" (nf (jackDChunk n mean) rs)
+    ]
 
---         bench "jackknife" (nf (jackknife mean) rs),
-    --    bench "jackknife" (nf (jackknife mean) rs)
---    bench "A" (nf (jackA mean) rs),
---    bench "A" (nf (jackA mean) rs)
- {-
-    --      bench "A" (nf (jackA mean) rs),
-    --      bench "ADiv" (nf (jackADiv 4 mean) rs),
-    --      bench "AChunk" (nf (jackAChunk n mean) rs),
-    --      bench "AChunk" (nf (jackAChunk n mean) rs)
-  -}
---    bench "BChunk" (nf (jackBChunk n mean) rs),
---    bench "B" (nf (jackB mean) rs)
-          {-
---          bench "B" (nf (jackB mean) rs),
---         bench "BDiv" (nf (jackBDiv 4 mean) rs),
-          bench "BChunk" (nf (jackBChunk n mean) rs),
-          bench "BChunk" (nf (jackBChunk n mean) rs)
-    --      -}
+benchmain2 args = do
+  let n = read (head args) :: Int
 
-    --      bench "BeChunk" (nf (jackBeChunk n mean) rs),
-    --      bench "Be" (nf (jackBe mean) rs)
-          {-
-          bench "Be" (nf (jackBe mean) rs),
-          bench "BeDiv" (nf (jackBeDiv 4 mean) rs),
-          bench "BeChunk" (nf (jackBeChunk n mean) rs)
-          -}
-    --      bench "CChunk" (nf (jackCChunk n mean) rs),
-    --      bench "C" (nf (jackC mean) rs)
-    --      {-
-    --      bench "C" (nf (jackC mean) rs),
-    --      bench "CDiv" (nf (jackCDiv 4 mean) rs),
-    --      bench "CChunk" (nf (jackCChunk n mean) rs),
-    --      bench "CChunk" (nf (jackCChunk n mean) rs)
-    --      -}
-    --    bench "DChunk" (nf (jackDChunk n mean) rs),
-    --    bench "D" (nf (jackD mean) rs)
-        {-
-    --    bench "D" (nf (jackD mean) rs),
-    --    bench "D" (nf (jackD mean) rs)
-    --    bench "DDiv" (nf (jackDDiv 4 mean) rs),
-        bench "DChunk" (nf (jackDChunk n mean) rs),
-        bench "DChunk" (nf (jackDChunk n mean) rs)
-        -}
-    --    bench "msort" (nf (mergeSort n) rs)
-        bench "search" (nf (search n 0) ls)
+  let xs = take 60000 (randoms (mkStdGen 211570155)) :: [Int]
+  
+  let i = 426423478
+  let ys = take 3000 xs ++ [i] ++ drop 3001 xs
 
-                                                                          ]
-
-
+  withArgs (drop 1 args) $ defaultMain [
+    bench "MergeSort" (nf (mergeSort False n) xs),
+    bench "MergeSortPar" (nf (mergeSort True n) xs),
+    bench "Search" (nf (search False n i) ys),
+    bench "SearchPar" (nf (search True n i) ys)
+    ]
 
 --A
 jackA :: (NFData b) => ([a] -> b) -> [a] -> [b]
 jackA f = amap f . resamples 500
-
-jackADiv :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
-jackADiv n f = divideMap n (amap (map f)) . resamples 500
 
 jackAChunk :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
 jackAChunk n f = chunkMap n (amap (map f)) . resamples 500
@@ -141,29 +106,20 @@ jackAChunk n f = chunkMap n (amap (map f)) . resamples 500
 jackB :: (NFData b) => ([a] -> b) -> [a] -> [b]
 jackB f = bmap f . resamples 500
 
-jackBDiv :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
-jackBDiv n f = divideMap n (bmap (map f)) . resamples 500
-
 jackBChunk :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
 jackBChunk n f = chunkMap n (bmap (map f)) . resamples 500
 
 --Be
 
-jackBeDiv :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
-jackBeDiv n f = divideMap n (MP.runPar . MP.parMap (map f)) . resamples 500
+jackBe :: (NFData b) => ([a] -> b) -> [a] -> [b]
+jackBe f = MP.runPar . MP.parMap f . resamples 500
 
 jackBeChunk :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
 jackBeChunk n f = chunkMap n (MP.runPar . MP.parMap (map f)) . resamples 500
 
-jackBe :: (NFData b) => ([a] -> b) -> [a] -> [b]
-jackBe f = MP.runPar . MP.parMap f . resamples 500
-
 -- C
 jackC :: (NFData b) => ([a] -> b) -> [a] -> [b]
 jackC f = cmap f . resamples 500
-
-jackCDiv :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
-jackCDiv n f = divideMap n (cmap (map f)) . resamples 500
 
 jackCChunk :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
 jackCChunk n f = chunkMap n (cmap (map f)) . resamples 500
@@ -171,9 +127,6 @@ jackCChunk n f = chunkMap n (cmap (map f)) . resamples 500
 -- D
 jackD :: (NFData b) => ([a] -> b) -> [a] -> [b]
 jackD f = dmap f . resamples 500
-
-jackDDiv :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
-jackDDiv n f = divideMap n (dmap (map f)) . resamples 500
 
 jackDChunk :: (NFData b) => Int -> ([a] -> b) -> [a] -> [b]
 jackDChunk n f = chunkMap n (dmap (map f)) . resamples 500
@@ -210,27 +163,20 @@ chunk :: Int -> [a] -> [[a]]
 chunk n [] = []
 chunk n xs = as : chunk n bs where (as, bs) = splitAt n xs
 
-
 chunkMap :: (NFData b) => Int -> ([[a]] -> [[b]]) -> [a] -> [b]
 chunkMap n mapF = concat . mapF . chunk n
 
-
-divide :: Int -> [a] -> [[a]]
-divide n xs = chunk d xs
-    where d = ceiling (len / fromIntegral n)
-          len = fromIntegral $ length xs
-
-divideMap :: (NFData b) => Int -> ([[a]] -> [[b]]) -> [a] -> [b]
-divideMap n mapF = concat . mapF . divide n
-
 -- 2 ------------------------------------------------------------
+divConq :: (NFData b) => (a -> b) -> a -> (a -> Maybe [a]) -> ([b] -> b) -> b
+divConq f arg divide combine  = go arg
+    where
+        go arg =
+            case divide arg of
+                Nothing -> f arg
+                Just xs -> combine $ map go xs
 
---divConq :: (a -> b) -> a -> (a -> [a]) -> ([b] -> b) -> b
---divConq f arg divide combine  = combine $ map f (devide arg)
-
-
-divConq :: (NFData b) => (a -> b) -> a -> (a -> Bool) -> (a -> Maybe [a]) -> ([b] -> b) -> b
-divConq f arg threshold divide combine  = go arg
+divConqPar :: (NFData b) => (a -> b) -> a -> (a -> Bool) -> (a -> Maybe [a]) -> ([b] -> b) -> b
+divConqPar f arg threshold divide combine  = go arg
     where
         go arg =
             case divide arg of
@@ -238,21 +184,14 @@ divConq f arg threshold divide combine  = go arg
                 Just xs -> combine $ mapF go xs
 
                 where mapF | threshold arg = map
-                           | otherwise     = map
+                           | otherwise     = dmap
 
-
-
-sortL :: (Ord a) => [a] -> [a] -> [a]
-sortL [] yl = yl
-sortL xl [] = xl
-sortL (x:xs) (y:ys) | x <= y = x : sortL xs (y:ys)
-                   | otherwise = y : sortL (x:xs) ys
-
-mergeSort :: (Ord a,NFData a) => Int -> [a] -> [a]
-mergeSort d xs = divConq f xs threshold divide combine
+mergeSort :: (Ord a,NFData a) => Bool -> Int -> [a] -> [a]
+mergeSort usePar d xs = if usePar then divConqPar f xs threshold divide combine
+                        else divConq f xs divide combine
     where
         f :: Ord a => [a] -> [a]
-        f x = x
+        f = id
 
         threshold :: [a] -> Bool
         threshold x = length x < d
@@ -269,10 +208,11 @@ mergeSort d xs = divConq f xs threshold divide combine
                              | otherwise = y : combine [x:xs, ys]
 
 
-search :: (Eq a, NFData a) =>  Int -> a -> [a] -> Bool
-search d i xs = divConq f xs threshold divide combine
+search :: (Eq a, NFData a) => Bool -> Int -> a -> [a] -> Bool
+search usePar d i xs = if usePar then divConqPar f xs threshold divide combine
+                       else divConq f xs divide combine
     where
-        --f :: Eq a => [a] -> Bool
+        --f :: (Eq a, NFData a) => [a] -> Bool
         f = elem i
 
         combine :: [Bool] -> Bool
@@ -286,3 +226,5 @@ search d i xs = divConq f xs threshold divide combine
                         ([],l) -> Nothing
                         (l,[]) -> Nothing
                         (l1,l2) -> Just [l1,l2]
+
+-- 3 ------------------------------------------------------------
