c947a6c9a321771bc51826a3da7348335a054187
tehAndrew <andpa_kontakt@live.se>
2022-04-19 15:25:49 +0200

    Polling etch added

--- LabB/sudoku.erl
+++ LabB/sudoku.erl
@@ -91,6 +91,7 @@ refine(M) ->
 		unblocks(
 		  refine_rows(
 		    blocks(M))))))),
+    %% No use parallelizing NewM since we still need to wait for its value
     if M==NewM ->
 	    M;
        true ->
@@ -98,7 +99,12 @@ refine(M) ->
     end.
 
 refine_rows(M) ->
-    lists:map(fun refine_row/1,M).
+    %%Status = pool_send_work(fun() -> catch lists:map(fun refine_row/1, M) end),
+    %%pool_wait_for_value(Status).
+
+    %map_pool_chunk(9, fun(X) -> catch refine_row(X) end, M).
+    
+    lists:map(fun refine_row/1, M).
 
 refine_row(Row) ->
     Entries = entries(Row),
@@ -165,10 +171,15 @@ guess(M) ->
 
 %% given a matrix, guess an element to form a list of possible
 %% extended matrices, easiest problem first.
+solve_guesses_pool([], _, _, _) -> [];
+solve_guesses_pool([G|Gs], I, J, M) ->
+    Status = pool_send_work(fun() -> solve_guesses_pool(Gs, I, J, M) end),
+    [catch refine(update_element(M,I,J,G))] ++ pool_wait_for_value(Status).
 
 guesses(M) ->
     {I,J,Guesses} = guess(M),
-    Ms = [catch refine(update_element(M,I,J,G)) || G <- Guesses],
+    Ms = solve_guesses_pool(Guesses, I, J, M),
+    %%Ms = [catch refine(update_element(M,I,J,G)) || G <- Guesses],
     SortedGuesses =
 	lists:sort(
 	  [{hard(NewM),NewM}
@@ -205,10 +216,10 @@ solve_refined(M) ->
 	true ->
 	    M;
 	false ->
-	    solve_one(guesses(M))
+	    solve_one(guesses(M))   %todo
     end.
 
-solve_one([]) ->
+solve_one([]) ->                %todo
     exit(no_solution);
 solve_one([M]) ->
     solve_refined(M);
@@ -230,27 +241,16 @@ bm(F) ->
 repeat(F) ->
     [F() || _ <- lists:seq(1,?EXECUTIONS)].
 
-benchmarks([]) -> [];
-benchmarks([{Name, Puzzle}|Puzzles]) ->
-    Parent = self(),
-    Ref = make_ref(),
-    spawn_link(fun() ->
-        Parent ! {Ref, benchmarks(Puzzles)}
-    end),
-    [{Name, bm(fun() -> solve(Puzzle) end)}] ++ 
-        receive {Ref, Solutions} -> Solutions end.
-
-benchmarksSeq(Puzzles) ->
-    [{Name,bm(fun()->solve(M) end)} || {Name,M} <- Puzzles].
-
+benchmarks(Puzzles) ->
+    %%start_pool(erlang:system_info(schedulers) - 1),
+    start_pool(6),
+    Res = [{Name,bm(fun()->solve(M) end)} || {Name,M} <- Puzzles],
+    pool ! {stop,self()},
+    receive {pool,stopped} -> Res end.
 
 benchmarks() ->
   {ok,Puzzles} = file:consult("problems.txt"),
   timer:tc(?MODULE,benchmarks,[Puzzles]).
-
-benchmarksSeq() ->
-  {ok,Puzzles} = file:consult("problems.txt"),
-  timer:tc(?MODULE,benchmarksSeq,[Puzzles]).
 
 %% check solutions for validity
 
@@ -265,6 +265,137 @@ valid_solution(M) ->
 
 
 %% 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+benchmarks_par([]) -> [];
+benchmarks_par([{Name, Puzzle}|Puzzles]) ->
+    Thread = start_thread(fun() -> benchmarks_par(Puzzles) end),
+    [{Name, bm(fun() -> solve(Puzzle) end)}] ++ wait_for_value(Thread).
 
+benchmarks_par() ->
+  {ok,Puzzles} = file:consult("problems.txt"),
+  timer:tc(?MODULE,benchmarks_par,[Puzzles]).
 
 %% 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% HELPERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Regular thread starting---------------------------------------------------
+start_thread(F) ->
+    Parent = self(),
+    Pid = spawn_link(fun() -> Parent ! {self(),F()} end),
+    {spawned, Pid}.
+
+wait_for_value({spawned, Pid}) ->
+    receive {Pid, Value} -> Value end.
+
+%% Parallel mapping----------------------------------------------------------
+chunk(N, XS) ->
+    case N < length(XS) of
+        true ->
+            {LS, RS} = lists:split(N, XS),
+            [LS] ++ chunk(N, RS);
+        false ->
+            [XS]
+    end.
+
+map_par_chunk(N, F, XS) ->
+    Chunks = chunk(N, XS),
+    map_par_chunk_inner(F, Chunks).
+
+map_par_chunk_inner(_, []) -> [];
+map_par_chunk_inner(F, [X|XS]) ->
+    Thread = start_thread(fun() -> map_par_chunk_inner(F, XS) end),
+    lists:map(F, X) ++ wait_for_value(Thread).
+
+map_pool_chunk(N, F, XS) ->
+    Chunks = chunk(N, XS),
+    map_pool_chunk_inner(F, Chunks).
+
+map_pool_chunk_inner(_, []) -> [];
+map_pool_chunk_inner(F, [X|XS]) ->
+    Status = pool_send_work(fun() -> map_pool_chunk_inner(F, XS) end),
+    lists:map(F, X) ++ pool_wait_for_value(Status).
+
+%% Pooling-------------------------------------------------------------------
+start_pool(N) ->
+    %% Register the spawned thread to the atom 'pool'. Spawn thread thad calls
+    %% pool on N worker threads.
+    true = register(
+        pool, 
+        spawn_link(
+            fun() -> pool([worker() || _ <- lists:seq(1, N)]) end
+        )
+    ).
+
+%% Entry function to the worker pool.
+pool(Workers) ->
+    pool(Workers, Workers).
+
+%% The pool function waits idly for a message.
+%% Avaliable is the available workers and All is all workers.
+pool(Avaliable, All) ->
+    receive 
+    %% Try get a worker. Send the Pid of the process asking.
+    {get_worker, Pid} ->
+        case Avaliable of
+            %% When no worker is avaliable, notify the sender
+            %% process.
+            [] ->
+                Pid ! {pool, no_worker},
+                pool(Avaliable, All); %% Continue 
+            %% Otherwise, take a worker and send it back to the
+            %% sender process.
+            [W|Ws] ->
+                Pid ! {pool, W},
+                pool(Ws, All) %% Continue with one less avaliable worker.
+        end;
+    %% Take worker W, sent by a process, and continue with tat worker now
+    %% avaliable.
+    {return_worker, W} ->
+        pool([W|Avaliable], All);
+    %% Kill the pool. Requested by Pid.
+    {stop, Pid} ->
+        [unlink(W) || W <- All], %% Unlink all workers from pool so we can terminate them all before the pool is terminated.
+        [exit(W, kill) || W <- All], %% Sent exit signal to all workers with reason 'kill'.
+        unregister(pool), %% Stop associating the atom pool with this process.
+        Pid ! {pool, stopped} %% Notify sender process and then terminate normally.
+    end.
+
+%% Spawn worker thread with function work.
+worker() ->
+    spawn_link(fun work/0).
+
+%% Waits idly to recieve a task.
+work() ->
+    receive
+    %% A task is defined by the sender process Pid, a unique reference and the work itself (a function).
+    {task, Pid, Ref, F} ->
+        Pid ! {Ref, F()}, %% Send the result of the work back to the sender process.
+        catch pool ! {return_worker, self()}, %% Return to the pool. Error might occur because pool may not be registered? TODO: check this
+        work() %% Continue wait for tasks.
+    end.
+
+%% Send work F to the pool
+pool_send_work(F) ->
+    %% Check if pool is created.
+    case whereis(pool) of
+        undefined ->
+            ok; %% If not, return ok
+        %% Get a worker from the pool otherwise.
+        Pool -> Pool ! {get_worker, self()}
+    end,
+    receive
+        %% Return work if no worker was avaliable.
+        {pool, no_worker} ->
+            {not_spawned, F};
+        %% If worker was avaliable, send it work
+        {pool, W} ->
+            Ref = make_ref(),
+            W ! {task, self(), Ref, F},
+            {spawned, Ref} %% Return reference so result can be fetched
+    end.
+
+%% If pool could not provide worker, do work in this process.
+pool_wait_for_value({not_spawned, F}) ->
+    F();
+%% Otherwise, wait for result.
+pool_wait_for_value({spawned, R}) ->
+    receive {R, X} -> X end.
